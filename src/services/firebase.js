9f3db519b0a84d63a74d277b94671cf7ec82de88
shiinedev <shiinecadde17@gmail.com>
2024-07-15 19:10:49 +0300

    updated the api key

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey:"AIzaSyCbbParHS-jrA0DSrt-zX2l64yj7NIWpkU",
+    authDomain:"bookmarkmanegar.firebaseapp.com",
+    projectId:"bookmarkmanegar",
+    storageBucket: "bookmarkmanegar.appspot.com,",
+    messagingSenderId: "179785553401",
+    appId: "1:179785553401:web:0509daa0b771a6002a5928"
   };
 
 const app = initializeApp(firebaseConfig);
